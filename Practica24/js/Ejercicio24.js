10f6151a90e50c4881aa248c1b1a84d57dee3711
kimberlytrj <kimberly.trejo@uabc.edu.mx>
2023-10-03 18:02:12 -0700

    Uso de JavaScript

---
+++ Practica24/js/Ejercicio24.js
@@ -0,0 +1,41 @@
+/*var numero1 = prompt("Ingrese el primer numero");
+var numero2 = prompt("Ingrese el segundo numero");
+var resultado = (numero1 + numero2);
+
+alert(resultado);*/
+
+
+/*var mensaje = "Bienvenido a nuestro sitio web";
+var texto_1 = " texto comillas dobles, ";
+var texto_2 = ' texto comillas simples...';
+
+alert (mensaje + texto_1 + texto_2);*/
+
+
+/*var clienteRegistrado = false;
+var ivaIncluido = true;
+
+alert(clienteRegistrado);*/
+
+
+/*var array = ['Tijuana', 'Rosarito', 'Ensenada', 'Mexicali'];
+
+alert(array);
+alert(array[3]);*/
+
+
+var num1 = prompt("Ingrese el primer numero");
+var num2 = prompt("Ingrese el segundo numero");
+
+var n1 = parseInt(num1);
+var n2 = parseInt(num2);
+
+var suma = n1 + n2;
+var resta = n1 - n2;
+var division = n1 / n2;
+var multiplicacion = n1 * n2;
+
+alert("Operaciones basicas: \n\nSuma: " + suma + "\nResta: " + resta + "\nDivisión: " + division + "\nMultiplicación: " + multiplicacion);
+
+
+/* KIMBERLY JAZMIN TREJO ESTRADA 372 */
