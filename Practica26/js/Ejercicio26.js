9bad9c46c357cd93206ff8c515854e647025183b
kimberlytrj <kimberly.trejo@uabc.edu.mx>
2023-10-09 19:29:01 -0700

    Uso de Java Script, variables y else if

---
+++ Practica26/js/Ejercicio26.js
@@ -0,0 +1,46 @@
+/*var num = 8;
+
+if (num < 10) {
+    console.log("El número es menor a 10");
+} else if (num > 10) {
+    console.log("El número es mayor a 10");
+} else {
+    console.log("El número es igual a 10");
+}*/
+
+/*var estacion = prompt("Escribe una estación en minusculas.");
+
+switch (estacion){
+    case "invierno":
+        alert("Todo es frío.");
+        break;
+
+    case "primavera":
+        alert("Se colorea el mundo.");
+        break;
+
+    case "verano":
+        alert("Las noches se hacen largas.");
+        break;
+
+    case "otoño":
+        alert("Donde había color ya no queda nada.");
+        break;
+    
+    default:
+        alert("Eso no es una estación.");
+        break;
+}*/
+
+var num1 = 5;
+var num2 = 8;
+
+if (num1 <= num2) {
+    console.log("El número 1 no es mayor que npumero 2");
+} else if (num2 <= 0){
+    console.log("El número 2 es positivo");
+}else if (++num1 < num2){
+    console.log("Incrementar uno en Número 1, no lo hace mayor que Número 2");
+}
+
+/* KIMBERLY JAZMIN TREJO ESTRADA 372 */
